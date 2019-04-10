badd145bb8570862044655986e273bf985aeb31c
MatthewBaldock <matt.w.g.baldock@gmail.com>
2019-04-10 11:08:33 -0700

    Migration edits

--- database/migrations/2019_04_03_210106_modify_users_ver_2.php
+++ database/migrations/2019_04_03_210106_modify_users_ver_2.php
@@ -8,33 +8,33 @@ class ModifyUsersVer2 extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * composer require doctrine/dbal
      * @return void
      */
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
           
-			/*$table->timestamp('email_verified_at')->nullable();
-			$table->boolean('pref_clicking')->default('0');
-			$table->boolean('pref_link_thumb')->default('0');
-			$table->boolean('pref_media_preview')->default('0');
-			$table->boolean('pref_link_spotlight')->default('0');
-			$table->boolean('pref_link_trending_sub')->default('0');
-			$table->boolean('pref_link_recent')->default('0');
-			$table->boolean('pref_link_compress')->default('0');
-			$table->boolean('pref_link_hide_upvoted')->default('0');
-			$table->boolean('pref_link_downvoted')->default('0');
-			$table->boolean('pref_link_display')->default('0');
-			$table->boolean('pref_link_min_link_score')->default('0');
-			$table->boolean('pref_comments_ignore_suggested')->default('0');
-			$table->boolean('pref_comments_min')->default('0');
-			$table->boolean('pref_comments_display')->default('0');
-			$table->boolean('pref_message_show_conv_inbox')->default('0');
-			$table->boolean('pref_message_collapse')->default('0');
-			$table->boolean('pref_message_mark_read')->default('0');
-			$table->boolean('pref_message_notify')->default('0');
-			$table->boolean('pref_content_include_nsfw')->default('0');
+			$table->dropColumn('email_verified_at');
+			$table->dropColumn('pref_clicking');
+			$table->dropColumn('pref_link_thumb');
+			$table->dropColumn('pref_media_preview');
+			$table->dropColumn('pref_link_spotlight');
+			$table->dropColumn('pref_link_trending_sub');
+			$table->dropColumn('pref_link_recent');
+			$table->dropColumn('pref_link_compress');
+			$table->dropColumn('pref_link_hide_upvoted');
+			$table->dropColumn('pref_link_downvoted');
+			$table->dropColumn('pref_link_display');
+			$table->dropColumn('pref_link_min_link_score');
+			$table->dropColumn('pref_comments_ignore_suggested');
+			$table->dropColumn('pref_comments_min');
+			$table->dropColumn('pref_comments_display');
+			$table->dropColumn('pref_message_show_conv_inbox');
+			$table->dropColumn('pref_message_collapse');
+			$table->dropColumn('pref_message_mark_read');
+			$table->dropColumn('pref_message_notify');
+			$table->dropColumn('pref_content_include_nsfw');
             */
         });
     }
